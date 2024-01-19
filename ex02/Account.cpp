372ef211f4bd6eb09e16d74efb8ec8e1270986ce
jmsmg <jmsmg1@me.com>
2024-01-19 10:18:12 +0900

    add Account.hpp tests.cpp

---
+++ ex02/Account.cpp
@@ -0,0 +1,25 @@
+
+static int  getNbAccounts()
+{
+
+}
+
+static int  getTotalAmount()
+{
+
+}
+
+static int  getNbDeposits()
+{
+
+}
+
+static int  getNbWithdrawals()
+{
+
+}
+
+static void displayAccountsInfos()
+{
+
+}
